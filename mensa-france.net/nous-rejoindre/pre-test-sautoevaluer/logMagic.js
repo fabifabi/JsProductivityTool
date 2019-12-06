6609ecb614edd63324b770887b840bea0ff7387b
Fabi <minteo1980@gmail.com>
2019-12-06 07:41:05 +0100

    a la recherche du onclik...

--- mensa-france.net/nous-rejoindre/pre-test-sautoevaluer/logMagic.js
+++ mensa-france.net/nous-rejoindre/pre-test-sautoevaluer/logMagic.js
@@ -21,12 +21,48 @@
         console.log("bidon", this);
     }
 
+    function recOn(elm) {
+        if (elm === undefined)
+            return;
+        for (var i = 0; i < elm.children.length; i++) {
+            recOn(elm.children[i]);
+        }
+        for (var i in elm) {
+            if (i.substr(0, 2) === "on" && typeof (elm[i]) === "function") {
+                var keep = elm[i];
+                elm[i + "new"] = function (keep, elm) {
+                    console.log(i, this);
+                    keep.bind(elm)();
+                }
+                // elm.onclick=fun;
+                // elm.onclick()
+                Object.defineProperty(elm, i, {
+                    set: function (x) {
+                        console.log("affectation", this);
+                        this[i + "new"] = function () {
+                            return function () {
+                                console.log(i, this);
+                                x();
+                            }
+                        }
+                    },
+                    get: function () {
+                        return this[i + "new"];
+                    }
+                });
+            }
+        }
+    }
+
+
     function run() {
         console.log("run");
         EventTarget.prototype.addEventListener2 = EventTarget.prototype.addEventListener;
         EventTarget.prototype.addEventListener = MyaddEventListener;
         document.getElementById("hop").addEventListener('click', bidon);
         document.getElementById("hop").addEventListener('click', bidon);
+        recOn(document.body);
+        document.getElementById("hop").onclick = bidon;
     }
 
 })();
