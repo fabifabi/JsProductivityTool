e0317740c17d96b0184a7b215c0f422b5fcec0e5
Fabi <minteo1980@gmail.com>
2019-12-12 12:46:08 +0100

    Log sample

---
+++ logMagic.js
@@ -0,0 +1,23 @@
+; (function () {
+    function initDebug() {
+        if (debug === undefined || debug === false)
+            window.log = function () { };
+        return;
+    }
+    window.log = function () {
+        {
+            var err = new Error();
+            var s = err.stack;
+            var l = s.split("\n");
+            var line = l[0].includes(":") ? l[1] : l[2]; // chrome put "error" on first line
+            var myarg = Array.prototype.slice.call(arguments);
+            console.log(line.trim());
+            for (var i = 0; i < myarg.length; i++) {
+                console.table(myarg[i]);
+            }
+        }
+    }
+})();
+
+var o = { a: 1, b: 2, c: { e: 3, k: 5 } };
+log("hop", o);
--- logMagicEvent.js
+++ logMagicEvent.js
@@ -1,41 +1,38 @@
-//; (function log() {
-function initDebug() {
-    if (debug !== true)
-        return;
-    // interception d'event auto
-    function callFun(str, fun, that) {
-        return function (arg) {
-            console.log("event:", str, that);
-            fun.bind(that)(arg);
+; (function () {
+    function initDebug() {
+        if (debug !== true)
+            return;
+        // interception d'event auto
+        function callFun(str, fun, that) {
+            return function (arg) {
+                console.log("event:", str, that);
+                fun.bind(that)(arg);
+            }
+        }
+
+        function MyaddEventListener(str, fun, x) {
+            console.log("add event", str);
+            this.addEventListener2(str, callFun(str, fun, this), x);
+        }
+
+        EventTarget.prototype.addEventListener2 = EventTarget.prototype.addEventListener;
+        EventTarget.prototype.addEventListener = MyaddEventListener;
+    }
+
+    var log = function () {
+        {
+            var err = new Error();
+            var s = err.stack;
+            var l = s.split("\n");
+            var line = l[0].includes(":") ? l[1] : l[2]; // chrome put "error" on first line
+            var myarg = Array.prototype.slice.call(arguments);
+            console.log(line.trim());
+            for (var i = 0; i < myarg.length; i++) {
+                console.table(myarg[i]);
+            }
         }
     }
-
-    function MyaddEventListener(str, fun, x) {
-        console.log("add event", str);
-        this.addEventListener2(str, callFun(str, fun, this), x);
-    }
-
-    EventTarget.prototype.addEventListener2 = EventTarget.prototype.addEventListener;
-    EventTarget.prototype.addEventListener = MyaddEventListener;
-}
-
-var log = function () {
-    {
-        var err = new Error();
-        var s = err.stack;
-        var l = s.split("\n");
-        var line = l[0].includes(":") ? l[1] : l[2]; // chrome put "error" on first line
-        var myarg = Array.prototype.slice.call(arguments);
-        console.log(line.trim());
-        for (var i = 0; i < myarg.length; i++) {
-            console.table(myarg[i]);
-        }
-    }
-}
-/*
-    return log;
-
-})();*/
+})();
 
 initDebug();
 
